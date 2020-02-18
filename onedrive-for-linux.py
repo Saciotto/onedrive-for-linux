528aee303728d3de40f34ef1fbb17c96695bb993
Matheus Rossi Saciotto <saciotto@gmail.com>
2020-02-18 01:04:29 -0300

    Add Onedrive class to handle token

--- onedrive-for-linux.py
+++ onedrive-for-linux.py
@@ -1,11 +1,9 @@
-from OAuth2Handler import OAuth2Handler
-from http.server import HTTPServer
-import webbrowser
+#!/usr/bin/python3
 
-url = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=c22bd74f-da4c-460d-af0a-f97aa232a908&scope=Files.ReadWrite%20Files.ReadWrite.all%20Sites.ReadWrite.All%20offline_access&response_type=code&redirect_uri=http://localhost:8000"
-webbrowser.open(url)
+from onedrive import Onedrive
 
-httpd = HTTPServer(('localhost', 8000), OAuth2Handler)
-httpd.handle_request()
-
-print(httpd.access_token)
+onedrive = Onedrive()
+onedrive.new_access_token()
+print(onedrive.access_token)
+onedrive.redeem_token()
+print(onedrive.authorization_code)
