b642b33b3e41d500efc79c1955ac3cd126d5ee10
Matheus Rossi Saciotto <saciotto@gmail.com>
2020-03-10 20:34:12 -0300

    Add database tests in main file

--- onedrive_for_linux/__main__.py
+++ onedrive_for_linux/__main__.py
@@ -1,15 +1,35 @@
 
 from .onedrive_api import OnedriveApi, onedrive_login
+from .database import Database
 
-onedrive2 = onedrive_login()
-account = onedrive2.get_account()
+my_account = None
+with Database() as db:
+    accounts = db.load_all_accounts()
+    print(accounts)
+    print(type(accounts))
+    for account in accounts:
+        if account[1] == "Matheus Rossi Saciotto":
+            my_account = account
+
+if not my_account:
+    onedrive = onedrive_login()
+    account = onedrive.get_account()
+else:
+    onedrive = OnedriveApi(my_account)
+    my_account = onedrive.get_account()
+
+with Database() as db:
+    db.save_account(account)
 
 onedrive = OnedriveApi(account)
+
+print(account)
 
 default_drive =  onedrive.get_defualt_drive()
 drive_id = default_drive['id']
 print(drive_id)
 
+default_root = onedrive.get_defualt_root()
 default_root = onedrive.get_defualt_root()
 file_id = default_root['id']
 print(file_id)
