3241962aa7b072530bd7568af482ebd8d86f752b
Matheus Rossi Saciotto <saciotto@gmail.com>
2020-04-09 20:12:37 -0300

    Add upload class

---
+++ onedrive_for_linux/upload.py
@@ -0,0 +1,14 @@
+
+class UploadSession:
+
+    def __init__(self, onedrive, session_filepath):
+        pass
+
+    def upload(self, local_path, parent_drive_id, parent_id, filename, etag):
+        pass
+
+    def restore(self):
+        pass
+
+    def upload_2(self):
+        pass
