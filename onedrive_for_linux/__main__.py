88e8bf3a833a9e834598d8ce256a739f4f3f9f1e
Matheus Rossi Saciotto <saciotto@gmail.com>
2020-04-09 19:19:52 -0300

    Add main entry point

--- onedrive_for_linux/__main__.py
+++ onedrive_for_linux/__main__.py
@@ -1,42 +1,23 @@
-
-from .onedrive import Onedrive
 from .cli_login import onedrive_login
+from .onedrive import Onedrive
 from .onedrive_account import OnedriveAccount
 from .onedrive_account_db import OnedriveAccountDB
 
-my_account = None
+
+def perform_sync():
+    print('Perform sync')
+
+account_db = None
 
 with OnedriveAccountDB() as db:
     accounts = db.load_all()
-    print(accounts)
-    print(type(accounts))
+    if len(accounts) > 0:
+        account_db = accounts[0]
 
-    for account in accounts:
-        if account[1] == "Matheus Rossi Saciotto":
-            my_account = account
-
-if not my_account:
+if not account_db:
     onedrive = onedrive_login()
     account = OnedriveAccount(onedrive)
 else:
-    account = OnedriveAccount.from_database(my_account)
+    account = OnedriveAccount.from_database(account_db)
 
-print(account)
-
-default_drive =  account.onedrive.get_defualt_drive()
-drive_id = default_drive['id']
-print(drive_id)
-
-default_root = account.onedrive.get_defualt_root()
-file_id = default_root['id']
-print(file_id)
-
-delta = account.onedrive.view_changes_by_id(drive_id, file_id)
-with open('delta.json', 'w') as fp:
-    print(delta, file=fp)
-
-with open('results.txt', 'w') as fp:
-    print('Changes by path root:', account.onedrive.view_changes_by_path(), file=fp)
-
-with OnedriveAccountDB() as db:
-    db.save(account.to_database())
+perform_sync()
---
+++ tests/test_integration.py
@@ -0,0 +1,42 @@
+
+from onedrive_for_linux.onedrive import Onedrive
+from onedrive_for_linux.cli_login import onedrive_login
+from onedrive_for_linux.onedrive_account import OnedriveAccount
+from onedrive_for_linux.onedrive_account_db import OnedriveAccountDB
+
+my_account = None
+
+with OnedriveAccountDB() as db:
+    accounts = db.load_all()
+    print(accounts)
+    print(type(accounts))
+
+    for account in accounts:
+        if account[1] == "Matheus Rossi Saciotto":
+            my_account = account
+
+if not my_account:
+    onedrive = onedrive_login()
+    account = OnedriveAccount(onedrive)
+else:
+    account = OnedriveAccount.from_database(my_account)
+
+print(account)
+
+default_drive =  account.onedrive.get_defualt_drive()
+drive_id = default_drive['id']
+print(drive_id)
+
+default_root = account.onedrive.get_defualt_root()
+file_id = default_root['id']
+print(file_id)
+
+delta = account.onedrive.view_changes_by_id(drive_id, file_id)
+with open('delta.json', 'w') as fp:
+    print(delta, file=fp)
+
+with open('results.txt', 'w') as fp:
+    print('Changes by path root:', account.onedrive.view_changes_by_path(), file=fp)
+
+with OnedriveAccountDB() as db:
+    db.save(account.to_database())
