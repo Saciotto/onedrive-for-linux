7f443e18133b13eb8446e79be57c4182e374d4de
Matheus Rossi Saciotto <saciotto@gmail.com>
2020-04-09 20:12:21 -0300

    Add selective class

---
+++ onedrive_for_linux/selective.py
@@ -0,0 +1,14 @@
+
+class SelectiveSync:
+
+    def load(self, path):
+        pass
+
+    def set_mask(self, mask):
+        pass
+
+    def is_name_excluded(self, name):
+        pass
+
+    def is_path_excluded(self, path):
+        pass
