ee3f9526fc981fb8b156100534c9c7387e7c720a
Matheus Rossi Saciotto <saciotto@gmail.com>
2020-04-09 19:18:39 -0300

    Add sync engine class

---
+++ onedrive_for_linux/sync.py
@@ -0,0 +1,17 @@
+
+
+class SyncEngine:
+    def __init__(self, cfg, onedrive, itemdb, selectiveSync):
+        pass
+
+    def apply_differences(self):
+        pass
+
+    def scan_for_differences(self, path = '.'):
+        pass
+
+    def upload_move_item(self, file_from, file_to):
+        pass
+
+    def delete_by_path(self, path):
+        pass
