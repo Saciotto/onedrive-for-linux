8c5444f3444964cf0ba8fa2e176099ee7a589b06
Matheus Rossi Saciotto <saciotto@gmail.com>
2020-04-09 20:11:57 -0300

    Add monitor class

---
+++ onedrive_for_linux/monitor.py
@@ -0,0 +1,15 @@
+
+class Monitor:
+
+    def __init__(self, selective_sync):
+        pass
+
+    def shutdown(self):
+        pass
+
+    def add(self, path):
+        pass
+
+    def update(self):
+        pass
+
