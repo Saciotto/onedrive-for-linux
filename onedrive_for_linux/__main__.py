66846521e29433fb08a54ba0dca2ebe9177d11b6
Matheus Rossi Saciotto <saciotto@gmail.com>
2020-04-10 21:33:11 -0300

    Add sync command

--- onedrive_for_linux/__main__.py
+++ onedrive_for_linux/__main__.py
@@ -4,12 +4,18 @@ from argparse import ArgumentParser
 from . import version
 from .cli_login import onedrive_login
 from .onedrive_account import OnedriveAccount
-
+from .sync import SyncEngine
 
 def login(args):
     onedrive = onedrive_login()
     account = OnedriveAccount(args.name, onedrive)
     account.save()
+
+
+def sync(args):
+    account = OnedriveAccount.load(args.name)
+    sync_engine = SyncEngine(account)
+    sync_engine.apply_differences()
 
 
 def download(args):
@@ -42,6 +48,10 @@ subparser.set_defaults(func=login)
 
 subparser = subparsers.add_parser('logout', help='logout the current user')
 subparser.set_defaults(func=logout)
+
+subparser = subparsers.add_parser('sync', help='perform a full sync')
+subparser.add_argument('name', help='unique name for this account')
+subparser.set_defaults(func=sync)
 
 subparser = subparsers.add_parser('download', help='only download remote changes')
 subparser.set_defaults(func=download)
--- onedrive_for_linux/sync.py
+++ onedrive_for_linux/sync.py
@@ -1,13 +1,26 @@
-
+from .item import Item
 
 class SyncEngine:
-    def __init__(self, cfg, onedrive, itemdb, selectiveSync):
-        pass
+    def __init__(self, account):
+        self.onedrive = account.onedrive
+        self.folder = '~/onedrive'
 
     def apply_differences(self):
-        pass
+        self.default_drive_id = self.onedrive.get_defualt_drive()['id']
+        self.root_id = self.onedrive.get_defualt_root()['id']
+        self._apply_differences(self.default_drive_id, self.root_id)
 
-    def scan_for_differences(self, path = '.'):
+    def _apply_differences(self, drive_id, file_id):
+        changes = self.onedrive.view_changes_by_id(drive_id, file_id)
+        for item in changes['value']:
+            is_root = (file_id == item['id'])
+            self._apply_difference(item, file_id, is_root)
+
+    def _apply_difference(self, drive_item, drive_id, is_root):
+        item = Item(drive_item)
+        print(item)
+
+    def scan_for_differences(self, path='.'):
         pass
 
     def upload_move_item(self, file_from, file_to):
