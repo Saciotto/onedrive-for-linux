c69b006d33cf73ac4fd23331abedbee2a0b1bbeb
Matheus Rossi Saciotto <saciotto@gmail.com>
2020-04-11 12:19:30 -0300

    Update item class

--- onedrive_for_linux/item.py
+++ onedrive_for_linux/item.py
@@ -1,7 +1,15 @@
 from datetime import datetime, timezone
+from enum import Enum, auto
+
+
+class ItemType(Enum):
+    FILE = auto()
+    FOLDER = auto()
+    REMOTE = auto()
+    UNKNOWN = auto()
+
 
 class Item:
-
     def __init__(self, drive_item):
         self.id = drive_item['id']
         self.name = drive_item.get('name', None)
@@ -15,23 +23,32 @@ class Item:
             self.modified = datetime.now(tz=timezone.utc)
 
         if 'file' in drive_item:
-            self.type = 'file'
+            self.type = ItemType.FILE
         elif 'folder' in drive_item:
-            self.type = 'folder'
+            self.type = ItemType.FOLDER
         elif 'remote' in drive_item:
-            self.type = 'remote'
+            self.type = ItemType.REMOTE
         else:
-            self.type = 'unknown'
+            self.type = ItemType.UNKNOWN
 
         if not 'root' in drive_item and 'parentReference' in drive_item:
             self.drive_id = drive_item['parentReference']['driveId']
+        else:
+            self.drive_id = None
 
         if 'file' in drive_item and 'hashes' in drive_item['file']:
             hashes = drive_item['file']['hashes']
             self.crc32 = hashes.get('crc32Hash', None)
             self.sha1 = hashes.get('sha1Hash', None)
             self.quick_xor = hashes.get('quickXorHash', None)
+        else:
+            self.crc32 = self.sha1 = self.quick_xor = None
 
         if 'remote' in drive_item:
             self.remote_drive_id = drive_item['remoteItem']['parentReference']['driveId']
             self.remote_id = drive_item['remoteItem']['id']
+        else:
+            self.remote_drive_id = self.remote_id = None
+
+    def __str__(self):
+        return f'Item({self.type}, {self.name})'
